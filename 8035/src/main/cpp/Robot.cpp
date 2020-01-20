96756043d507c30c719188f6319e252b142d2124
met4000 <nathsharwor@hotmail.com>
2020-01-20 15:37:21 +0800

    Formatting

--- 8035/src/main/cpp/Robot.cpp
+++ 8035/src/main/cpp/Robot.cpp
@@ -183,8 +183,7 @@ void Robot::AutonomousInit() {
 void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {
-}
+void Robot::TeleopPeriodic() {}
 
 void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
